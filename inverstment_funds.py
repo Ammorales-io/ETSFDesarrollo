2e0c91cb1a124a14652a3f576fd1fae8f7b32fc1
Amartinez-h <estudiante.defessus@gmail.com>
2018-04-29 18:23:00 +0200

    Extracción de datos funcional. Presentación de datos en estado beta.

--- inverstment_funds.py
+++ inverstment_funds.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template
-import tools.csv_tools
+from flask import Flask, render_template, request
+from tools.csv_tools import get_csv_json
 
 app = Flask(__name__)
 
 @app.route("/")
 def output():
     return render_template('inverstment_funds.html')
+
+@app.route("/info", methods=["GET"])
+def get_info():
+    from_row = request.args .get("from")
+    to_row = request.args.get("to")
+    search_query = request.args.get("query")
+
+    if from_row is not None:
+        from_row = int(from_row)
+
+    if to_row is not None:
+        to_row = int(to_row)
+
+    if search_query is None:
+        search_query = ""
+
+    return get_csv_json('ETS Development Challenge 2018.csv', search_query, from_row, to_row)
 
 if __name__ == "__main__":
     app.run()
--- tools/csv_tools.py
+++ tools/csv_tools.py
@@ -1,24 +1,29 @@
+from json import JSONEncoder
 import csv
 
-
 def open_csv(file):
-            column_names = next(csv.reader(file))
-            return csv.DictReader(file, fieldnames = column_names)
+    column_names = next(csv.reader(file))
+    return csv.DictReader(file, fieldnames = column_names)
 
 
-def get_cvs_info(filepath, filterstring = "", from_row = None, to_row = None):
+def get_csv_info(filepath, filterstring = "", from_row = None, to_row = None):
     with open(filepath, newline = '')  as file:
         fileReader = open_csv(file)
 
         returned_info = []
 
         for row in fileReader:
-                if row['name'][:len(filterstring)] == filterstring:
-                    returned_info.append(row)
+            if row['name'][:len(filterstring)] == filterstring:
+                returned_info.append(row)
 
     return returned_info[from_row:to_row]
 
+
+def get_csv_json(filepath, filestring = "", from_row = None, to_row = None):
+    return JSONEncoder().encode(get_csv_info(filepath, filestring, from_row, to_row))
+
+
 if __name__ == "__main__":
-    result = get_cvs_info('ETS Development Challenge 2018.csv', from_row=5, to_row=10)
-    for row in result:
-        print(row)
+    result = get_csv_json('ETS Development Challenge 2018.csv', from_row=5, to_row=10)
+    
+    print(result)
